ef5aa0a8dc49f956a07a4dd5404f195697d3b4ff
jcji <302926124@qq.com>
2024-04-25 12:23:19 +0800

    perf：1、优化导航界面

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,25 +1,43 @@
 fetch('../assets/json/navigation.json')
   .then(res => res.json())
   .then(data => {
-    console.log('获取导航数据成功 --- ', data);
+    console.log('获取导航数据成功 --- ', data)
     createHTMLElementUI(data)
   })
   .catch(err => {
-    console.log('获取导航数据失败 --- ', err);
-  });
+    console.log('获取导航数据失败 --- ', err)
+  })
 
   // 创建界面标签元素
   function createHTMLElementUI(data) {
     var $naviWrapper = $(document).find('.navigation-wrapper')
     if ($naviWrapper) {
       $naviWrapper.append(`<div class='left-navi'></div>`)
-      $naviWrapper.append(`<div class='right-content'>222</div>`)
+      $naviWrapper.append(`<div class='right-content'></div>`)
 
       // 添加左侧导航
       var $leftNavi = $naviWrapper.find('.left-navi')
       if ($leftNavi) {
         $.each(data, (index, item) => {
-          $leftNavi.append(`<div class='left-navi-item'>${item.title}</div>`)
+          $leftNavi.append(`<div class='left-navi-item' index='${index}'>${item.title}</div>`)
+        })
+
+        // 监听点击事件
+        $leftNavi.on('click', '.left-navi-item', function() {
+          console.log('点击了 1--- ', $(this), $(this).index())
+        })
+      }
+
+      // 添加右侧内容
+      var $rightContent = $naviWrapper.find('.right-content')
+      if ($rightContent) {
+        $.each(data, (index, item) => {
+          $rightContent.append(`<div class='right-content-item' index='${index}}'></div>`)
+
+          var $rightContentItem = $rightContent.find('.right-content-item')
+          if ($rightContentItem) {
+
+          }
         })
       }
     }
